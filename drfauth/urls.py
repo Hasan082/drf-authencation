43ca1f196b02cb889e4a9f9ce9410a90b3d407e3
Md Hasanuzzaman <dr.has82@gmail.com>
2025-07-17 23:55:50 +0600

    API app created

--- drfauth/urls.py
+++ drfauth/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admin/', include("account.urls")),
+    path('', include("account.urls")),
+    path('', include("api.urls")),    
 ]
