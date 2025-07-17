f9044305c53b92d5a3cadfb45f3499b9c4354146
Md Hasanuzzaman <dr.has82@gmail.com>
2025-07-18 05:13:04 +0600

    Update apps.py

--- my_account/apps.py
+++ my_account/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class MyAccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'my_account'  
-
-    def ready(self):
-        import my_account.signals
